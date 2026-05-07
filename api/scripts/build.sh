0c56ec6e712b81bc6d3d71f919ed1e200e7b7832
Anand Kulkarni <anandkulkarni67@gmail.com>
2026-05-07 11:47:30 -0700

    update cicd scripts to include unit tests execution

--- api/scripts/build.sh
+++ api/scripts/build.sh
@@ -2,11 +2,15 @@
 
 artifact_version=$1
 
-echo "build_script"
+echo 'build_script'
 
 rm -rf *.zip
 
 npm version $artifact_version --no-git-tag-version
+if [ $? -ne 0 ]; then
+  echo 'Failed to update application version. Please input correct application version before initiating another build.'
+  exit 1
+fi
 
 cd api
 
@@ -14,14 +18,18 @@ rm -rf node_modules
 
 rm -rf dist
 
-npm version $artifact_version
-
 npm install
+
+npm run tests
+if [ $? -ne 0 ]; then
+  echo 'Tests failed. Please fix tests before initiating another build.'
+  exit 1
+fi
 
 npm run build
 
 application_version=$(npm --loglevel silent run version)
 
-zip -r "$application_version.zip" ./
+zip -r '$application_version.zip' ./
 
-echo "$(ls -l)"
+echo '$(ls -l)'
--- api/scripts/deploy.sh
+++ api/scripts/deploy.sh
@@ -8,6 +8,6 @@ echo 'deploy-script'
 
 cd api
 
-aws lambda update-function-code --function-name "$application_name-api-lambda" --zip-file fileb://$artifact_version.zip
+aws lambda update-function-code --function-name '$application_name-api-lambda' --zip-file fileb://$artifact_version.zip
 
 rm -rf $artifact_version.zip
